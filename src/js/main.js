a7b34b63ce151be1cd180c174d9ef3afa0da2c95
Rob Hitt <robhitt@gmail.com>
2018-02-14 00:11:53 -0500

    Create above the fold desktop

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,74 @@
-// document.addEventListener("DOMContentLoaded", function(){
-//   // Handler when the DOM is fully loaded
-// });
+window.onload = function () {
+  onResize();
 
-window.onload = function() {
-    onResize();
+  // Event Listener on window resize
+  // don't forget to add a debounce function
+  window.addEventListener("resize", onResize);
 
-    // Event Listener on window resize
-    // don't forget to add a debounce function
-    window.addEventListener("resize", onResize);
+  function onResize() {
+    let height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
-    function onResize() {
-        // console.log(this);
-        // var height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-        // console.log("Height", this.innerHeight);
-        // console.log("Width", this.innerWidth);
+    // grab middle container height to allow scrolling with overflow
+    const heroCenter = document.querySelector(".hero-center");
+    console.log(height);
+    heroCenter.style.height = height;
+  }
 
-        // grab middle container height to allow scrolling with overflow
-        let heroCenter = document.querySelector(".hero-center");
-        console.log(this.innerHeight);
-        
-        heroCenter.style.height = this.innerHeight;
-    }
+  // Toggle Modal Logic
+  const desktopImg = document.querySelectorAll(".img-desktop");
+  const modalImg = document.querySelectorAll(".img-modal");
+  desktopImg.forEach( image => image.addEventListener("click", toggleModal)); 
+  modalImg.forEach( image => image.addEventListener("click", toggleModal)); 
 
+  function toggleModal() {
+    const mainContainer = document.querySelector(".main-container");
+    const modal = document.querySelector(".modal");
+    console.log(mainContainer.style.display);
     
+    if (mainContainer.style.display === "none") {
+      mainContainer.style.display = "block";
+      modal.style.display = "none";
+    } else {
+      mainContainer.style.display = "none";
+      modal.style.display = "block";
+    }
+  }
+  
+  // Toggle product color picker
+  const colorBoxBlack = document.querySelector('.color-box-black');
+  const colorBoxBeige = document.querySelector('.color-box-beige');
+  colorBoxBlack.addEventListener("click", toggleColorPickerBlack);
+  colorBoxBeige.addEventListener("click", toggleColorPickerBeige);
+  const blackCheck = document.querySelector(".color-box-black-check");
+  const beigeCheck = document.querySelector(".color-box-beige-check");    
+
+  function toggleColorPickerBlack() {
+    blackCheck.classList.add("color-box-black-check-active");
+    beigeCheck.classList.remove("color-box-beige-check-active");
+  }
+
+  function toggleColorPickerBeige() {
+    blackCheck.classList.remove("color-box-black-check-active");
+    beigeCheck.classList.add("color-box-beige-check-active");
+  }
+
+  // Product quantity counter
+  let counter = 0;
+  const reduceProduct = document.querySelector(".quantity-reduce-product");
+  const addProduct = document.querySelector(".quantity-add-product");
+  reduceProduct.addEventListener("click", reduceQuantity);
+  addProduct.addEventListener("click", increaseQuantity);
+  const quantityCounter = document.querySelector(".quantity-counter");
+
+  function reduceQuantity() {
+    if (counter > 0) { 
+      counter -= 1 
+      quantityCounter.textContent = counter;    
+    }
+  }
+
+  function increaseQuantity() {
+    counter += 1;
+    quantityCounter.textContent = counter;    
+  }
 };
