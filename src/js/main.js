78269c8ab5903a83fb1358f778ff307e8327dec8
Rob Hitt <robhitt@gmail.com>
2018-02-15 13:25:35 -0500

    Add mobile carousel and logic

--- src/js/main.js
+++ src/js/main.js
@@ -9,42 +9,56 @@ window.onload = function () {
     let height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
     // grab middle container height to allow scrolling with overflow
-    const heroCenter = document.querySelector(".hero-center");
-    console.log(height);
+    const heroCenter = document.querySelector(".hero-desktop");
     heroCenter.style.height = height;
   }
 
   // Toggle Modal Logic
-  const desktopImg = document.querySelectorAll(".img-desktop");
+  const desktopImg = document.querySelectorAll(".pop-img");
   const modalImg = document.querySelectorAll(".img-modal");
-  desktopImg.forEach( image => image.addEventListener("click", toggleModal)); 
-  modalImg.forEach( image => image.addEventListener("click", toggleModal)); 
+  desktopImg.forEach(image => image.addEventListener("click", toggleModal));
+  modalImg.forEach(image => image.addEventListener("click", toggleModal));
 
   function toggleModal(event) {
     const mainContainer = document.querySelector(".main-container");
-    const modal = document.querySelector(".modal");
-    
-    if (mainContainer.style.display === "none") {
-      mainContainer.style.display = "block";
-      modal.style.display = "none";
-      window.scrollTo(0, 0);
-      
+    const modal = document.querySelector(".custom-modal");
+
+    if (window.innerWidth > 1024) {
+      if (mainContainer.style.display === "none") {
+        mainContainer.style.display = "block";
+        modal.style.display = "none";
+        window.scrollTo(0, 0);
+
+      } else {
+        let targetImage = event.currentTarget.dataset.img;
+        mainContainer.style.display = "none";
+        modal.style.display = "block";
+        const modalFocus = document.querySelector(`.modal-${targetImage}`);
+        modalFocus.scrollIntoView();
+      }
     } else {
-      let targetImage = event.currentTarget.dataset.img;
-      mainContainer.style.display = "none";
-      modal.style.display = "block";
-      const modalFocus = document.querySelector(`.modal-${targetImage}`);
-      modalFocus.scrollIntoView();
+      if (mainContainer.style.display === "none") {
+        mainContainer.style.display = "block";
+        modal.style.display = "none";
+        window.scrollTo(0, 0);
+      } else {
+        let targetImage = event.currentTarget.dataset.img;
+        mainContainer.style.display = "none";
+        console.log(targetImage);
+        modal.style.display = "block";
+        const modalFocus = document.querySelector(`.modal-${targetImage}`);
+        modalFocus.scrollIntoView();
+      }
     }
   }
-  
+
   // Toggle product color picker
   const colorBoxBlack = document.querySelector('.color-box-black');
   const colorBoxBeige = document.querySelector('.color-box-beige');
   colorBoxBlack.addEventListener("click", toggleColorPickerBlack);
   colorBoxBeige.addEventListener("click", toggleColorPickerBeige);
   const blackCheck = document.querySelector(".color-box-black-check");
-  const beigeCheck = document.querySelector(".color-box-beige-check");    
+  const beigeCheck = document.querySelector(".color-box-beige-check");
 
   function toggleColorPickerBlack() {
     blackCheck.classList.add("color-box-black-check-active");
@@ -65,16 +79,14 @@ window.onload = function () {
   const quantityCounter = document.querySelector(".quantity-counter");
 
   function reduceQuantity() {
-    if (counter > 0) { 
-      counter -= 1 
-      quantityCounter.textContent = counter;    
+    if (counter > 0) {
+      counter -= 1
+      quantityCounter.textContent = counter;
     }
   }
 
   function increaseQuantity() {
     counter += 1;
-    quantityCounter.textContent = counter;    
+    quantityCounter.textContent = counter;
   }
-
-  
 };
